3985591c8cca4f805227f2597ae0ea93a7b42e31
Carlos Forero <cefbursatil@gmail.com>
2021-11-06 11:55:36 -0500

    lista react

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {View } from 'react-native';
+import { List2 } from './Components/List/List2';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola soy coder!</Text>
-      <Text>Carlos Forero!</Text>
+    <View>
+      <List2/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
